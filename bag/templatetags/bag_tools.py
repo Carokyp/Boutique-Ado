6bd3303ec32f1109a3013c1aa6d2186b49dccdc7
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-04-13 17:47:48 +0100

    added update and remove to the bag

---
+++ bag/templatetags/bag_tools.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='cal_subtotal')
+def cal_subtotal(price, quantity):
+    """Calculate subtotal for a line item in the bag."""
+    return price * quantity
