23a5f05efa127c438bdd09f2ab1b693f34df622b
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-04-23 07:59:20 +0100

    Add cache control settings for AWS S3 object parameters

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -184,6 +184,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control settings
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=946080000',
+    }
     # Bucket settings
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
